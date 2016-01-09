c6b053ae4d7bfb2ae482b7ce2ac3c656571ca65a
OPersian <olena.persianova@gmail.com>
2016-01-09 23:05:48 +0200

    renamed apps

--- AppGraphic/GraphSettingsController.java
+++ AppGraphic/GraphSettingsController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package persaccounting.Graphic;
+package persaccounting.AppGraphic;
 
 import java.net.URL;
 import java.util.ResourceBundle;
