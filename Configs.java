3b7910bb7bd67eaf281bc1f69ec9b29ebd43f478
OPersian <olena.persianova@gmail.com>
2016-01-09 23:01:52 +0200

    added Graph app

--- Configs.java
+++ Configs.java
@@ -14,8 +14,7 @@ public class Configs {
     
     /*--------------------------navigation on scenes-------------------------*/ 
     public static final String ROOT_LAYOUT =    "/persaccounting/Views/RootLayout.fxml"; 
-    // public static final String MAIN_PAGE =      "/persaccounting/Calculator/CalculatorFXML.fxml"; 
-    public static final String MAIN_PAGE =      "/persaccounting/Views/MainPage.fxml"; // MainPage
+    public static final String MAIN_PAGE =      "/persaccounting/Views/MainPage.fxml";
     public static final String AUTHOR_INFO =    "/persaccounting/Views/AuthorInfo.fxml"; 
     public static final String CALCULATOR =     "/persaccounting/Calculator/CalculatorFXML.fxml"; 
     public static final String GRAPH_SETTINGS = "/persaccounting/Graphic/GraphSettings.fxml";
@@ -25,13 +24,23 @@ public class Configs {
     
     static String calculator_controller_path = 
             "/persaccounting/Calculator/CalculatorFXML.fxml";  // TODO: get rid of
-    
+
+    /*--------------------------------styling--------------------------------*/ 
+    // TODO: parametrize styling
+    public static String graph_default_style = 
+            "/persaccounting/StaticFiles/graph_default_style.css";
+    public static String graph_style_1 = 
+            "/persaccounting/StaticFiles/graph_style_1.css";
+    public static String graph_style_2 = 
+            "/persaccounting/StaticFiles/graph_style_2.css";   
+
     public static String calculator_default_style = 
-            "/persaccounting/StaticFiles/default_style.css";
+            "/persaccounting/StaticFiles/calc_default_style.css";
     public static String calculator_style_1 = 
-            "/persaccounting/StaticFiles/style_1.css";
+            "/persaccounting/StaticFiles/calc_style_1.css";
     public static String calculator_style_2 = 
-            "/persaccounting/StaticFiles/style_2.css";   
+            "/persaccounting/StaticFiles/calc_style_2.css";   
+    /*----------------------------END styling--------------------------------*/     
     
     
 }
--- Graphic/GraphDisplayController.java
+++ Graphic/GraphDisplayController.java
@@ -7,7 +7,10 @@ package persaccounting.Graphic;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 
 /**
  * FXML Controller class
@@ -15,13 +18,35 @@ import javafx.fxml.Initializable;
  * @author Olena
  */
 public class GraphDisplayController implements Initializable {
-
+    
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private LineChart<Double, Double> chart;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // TODO: implement GUI for startX inputting
+        double coefficient = 3, startX = 1, stopX = 100, step = 5;
+        chart.getData().add(initSerie(coefficient, startX, stopX, step));		
+    }   
+    
+    private XYChart.Series<Double, Double> initSerie(
+            double coefficient, 
+            double start, 
+            double stop, 
+            double delta) {		
+        XYChart.Series<Double, Double> series = new XYChart.Series();
+        // defining a series:
+        series.setName("Graph of y=k/x function");
+        // populating the series with data:
+        for (double i = start; i <= stop; i += delta) {	
+            System.out.println(i + "; " + coefficient/i);
+            series.getData().add(new XYChart.Data(i, coefficient/i));
+        }
+        return series;
     }    
     
 }
